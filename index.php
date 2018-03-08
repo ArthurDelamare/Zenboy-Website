4449d83c3c9b6754455ce5c2e690407a7d27e850
Unknown <arthur.delamare1@viacesi.fr>
2018-03-08 13:51:55 +0100

    ADD navbar

--- index.php
+++ index.php
@@ -7,14 +7,20 @@
 	<meta charset="utf-8" />
 </head>
 <body>
-	<header class="w3-container w3-teal w3-display-container w3-dark-grey" style="height: 8em">
+	<header class="w3-container w3-teal w3-display-container w3-dark-grey w3-text-light-grey" style="height: 8em">
 		<h1 class="w3-display-middle Font-Montserrat w3-xxlarge">Zenboy</h1>
 		<h2 class="w3-display-topleft Font-Montserrat" style="padding-left: 1em">Cisco Family</h2>
 		<h3 id="p1" onclick="myFunction()" class="w3-display-bottomright Font-Montserrat" style="padding-right: 1em">Roule un dragon</h3>
 	</header>
 
 	<nav>
-
+		<div class="w3-bar w3-grey">
+			<a href="#" class="w3-bar-item w3-button w3-large w3-text-white">Accueil</a>
+			<a href="#" class="w3-bar-item w3-button w3-large w3-text-white">Projets En Cours</a>
+			<a href="#" class="w3-bar-item w3-button w3-large w3-text-white">Projets Réalisés</a>
+			<a href="#" class="w3-bar-item w3-button w3-large w3-text-white">La Team</a>
+			<input type="text" class="w3-bar-item w3-input w3-light-grey w3-right" style="margin-right: 2em;" placeholder="Recherche...">
+		</div>
 	</nav>
 	<div class="w3-display-container w3-content w3-margin-top" style="width:65%">
 		<div class="w3-display-container mySlides w3-animate-opacity">
